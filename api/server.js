0e1e126fff0764e161d902ece36fc1baeeb233fd
Ravi Teja Chava <ravitejachava@Ravis-MBP.attlocal.net>
2022-07-09 19:44:24 -0700

    changes server port

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 8080;
+      port = 3080;
 
 // place holder for the data
 const users = [];
